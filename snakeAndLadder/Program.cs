6ec42a0f80b671e033badb18dae95b5ac63bff41
vundelayamini <vundelayamini@gmail.com>
2021-03-05 21:03:12 +0530

    [yamini]ADD: single player starting possition is zero

---
+++ snakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace snakeAndLadder
+{
+    class Program
+    { 
+       public const int START_POSSITION = 0;
+        public const int END_POSSITION = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Name.");
+            string player1 = Console.ReadLine();
+            int playerPosition =START_POSSITION;
+            Console.WriteLine("Current Position : " + playerPosition);
+        }
+    }
+}
