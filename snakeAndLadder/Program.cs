6dd8a41a51f432cc9ece3673a9ed260fae900251
vundelayamini <vundelayamini@gmail.com>
2021-03-07 22:49:48 +0530

    [yamini]ADD: Finally report which player is won

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -13,43 +13,82 @@ namespace snakeAndLadder
 
         static void Main(string[] args)
         {
-            int currPosition = STARTING_POSITION;
-            int moves = 0;
+            int player1Position = START_POSSITION;
+            int player2Position = START_POSSITION;
+            bool player1 = true;
             Console.WriteLine("Enter your name:");
-            Console.WriteLine("Player is at the currently starting position");
+            Console.WriteLine("Both Players are at the currently starting position");
 
-            while (currentPosition != WINNING_POSSITION)
+            while (player1Possition != WINNING_POSITION && player2Possition != WINNING_POSITION)
             {
-                Random random = new Random();
-                int diceValue = random.Next(1, 7);
-                Console.WriteLine("Player rolled : " + diceValue);
-                int actionTaken = random.Next(0, 3);
-                if (actionTaken == NO_PLAY)
+                if (player1)
                 {
-                    Console.WriteLine("No action taken");
-                }
-                else if (actionTaken == LADDER)
-                {
-                    currPosition += diceValue;
-                    if (currentPosition > WINNING_POSITION)
-                    {
-                        currentPosition -= diceValue;
-                    }
-                }
-                else
-                {
-                    currentPosition -= diceValue;
-                    if (currentPosition < STARTING_POSITION)
-                    {
-                        currentPosition = STARTING_POSITION;
-                    }
-                }
-                moves++;
-                Console.WriteLine(" The Player is at position is : " + currentPosition);
-            }
-            Console.WriteLine("Player reaches exact winning position in : " + moves + " moves");
+                    Random random = new Random();
+                    int diceValue = random.Next(1, 7);
+                    Console.WriteLine("Player rolled : " + diceValue);
+                    int actionTaken = random.Next(0, 3);
+                   if (actionTaken == NO_PLAY)
+                   {
+                        player1 = false;
+                   }
+                   else if (actionTaken == LADDER)
+                   {
+						player1Pos += diceValue;
+						if (player1Possition > WINNING_POSITION)
+						{
+							player1Possition = diceValue;
+						}
+					}
+					else
+					{
+						player1 = false;
+						player1Possition = diceValue;
+						if (player1Possition < START_POSITION)
+						{
+							player1Possition = START_POSITION;
+						}
+					}
+				}
+				else
+				{
+					Random random = new Random();
+					int diceValue = random.Next(1, 7);
+					Console.WriteLine("Player rolled : " + diceValue);
+					int actionTaken = random.Next(0, 3);
+					if (actionTaken == NO_PLAY)
+					{
+						player1 = true;
+					}
+					else if (actionTaken == LADDER)
+					{
+						player2Possition += diceValue;
+						if (player2Possition > 100)
+						{
+							player2Possition = diceValue;
+						}
+					}
+					else
+					{
+						player1 = true;
+						player2Possition = diceValue;
+						if (player2Possition < START_POSITION)
+						{
+							player2Possition = START_POSITION;
+						}
+					}
+				}
+			}
+			if (player1Possiton == WINNING_POSITION)
+			{
+				Console.WriteLine("Player 1 won the game");
+			}
+			else
+			{
+				Console.WriteLine("Player 2 won the game");
+			}
 
-        }
+
+		}
     }
 }
 
