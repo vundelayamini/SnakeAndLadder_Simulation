9ba8d0b78e09213f23d10b5da99be0882f8dd6a0
vundelayamini <vundelayamini@gmail.com>
2021-03-07 19:57:15 +0530

    [yamini]ADD: Checks for a option

--- snakeAndLadder/Program.cs
+++ snakeAndLadder/Program.cs
@@ -3,16 +3,48 @@
 namespace snakeAndLadder
 {
     class Program
-    { 
-       public const int START_POSSITION = 0;
+    {
+        public const int START_POSSITION = 0;
         public const int END_POSSITION = 100;
+        public const int NO_PLAY = 0;
+        public const int SNAKE = 1;
+        public const int LADDER = 2;
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter Your Name.");
+            Console.WriteLine("Enter your name");
             string player1 = Console.ReadLine();
-            int playerPosition =START_POSSITION;
-            Console.WriteLine("Current Position : " + playerPosition)
+            int playerPosition = START_POSSITION;
+            int diceRoll = RollDice();
+            Console.WriteLine("Dice Roll : " + diceRoll);
+            playerPosition = PlayerMovement(diceRoll, playerPosition);
+            Console.WriteLine("Your Position: " + playerPosition);
         }
-    }     
+        static int RollDice()
+        {
+            Random random = new Random();
+            int diceNum = random.Next(1, 7);
+            return diceNum;
+        }
+        static int PlayerMovement(int numberRolled, int playerPosition)
+        {
+            Random random = new Random();
+            int move = random.Next(0, 3);
+            switch (move)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("No Play");
+                    break;
+                case SNAKE:
+                    Console.WriteLine("Snake");
+                    playerPosition = playerPosition - numberRolled;
+                    break;
+                case LADDER:
+                    Console.WriteLine("Ladder");
+                    playerPosition = playerPosition + numberRolled;
+                    break;
+            }
+            return playerPosition;
+
+        }
+    }
 }
-    
